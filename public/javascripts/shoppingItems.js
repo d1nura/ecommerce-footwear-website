c2efe10028de1367455c18e731279c437e9309e4
d1nura <ddakshitha1@outlook.com>
2019-05-31 21:24:17 +0530

    after checout

--- public/javascripts/shoppingItems.js
+++ public/javascripts/shoppingItems.js
@@ -5,9 +5,11 @@ export function shoppingItems() {
   let shoppingItems = document.querySelector(".shoppingItems");
   let inc = shoppingItems.querySelectorAll("#inc");
   let dec = shoppingItems.querySelectorAll("#dec");
+  let itemPrice = document.querySelectorAll(".itmPrice");
 
   if (shoppingItems.children.length > 0) {
     console.log(itemDiv);
+    let tp = [];
 
     for (let i = 0; i < shoppingItems.children.length; i++) {
       let oPArr = [];
@@ -25,6 +27,8 @@ export function shoppingItems() {
         it.querySelector(".itmPrice").innerText =
           "$" +
           (oPArr[0] * parseInt(it.querySelector("#no").innerText)).toFixed(2);
+
+        //totalPrice.innerText =
       };
 
       dec[i].onclick = e => {
@@ -47,6 +51,15 @@ export function shoppingItems() {
             ).toFixed(2);
         }
       };
+
+      tp.push(removeDollar(itemPrice[i].innerText));
+      console.log(tp);
+
+      totalPrice.innerText = tp.reduce((p, n) => {
+        return (parseFloat(p) + parseFloat(n)).toFixed(2);
+      });
     }
+
+    //console.log(totalPrice.innerText);
   }
 }
