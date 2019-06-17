e5c5430c425bba9f3e1967154942ae6991123161
d1nura <ddakshitha1@outlook.com>
2019-06-17 15:16:41 +0530

    new index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "pug");
